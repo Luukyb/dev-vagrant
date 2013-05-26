6dc0997bc21637d2e4fe8a0ee5cb17a18deef110
Luc <luc@Lucs-MacBook-Pro.local>
2013-05-26 18:29:30 +0100

    Puppet manifest update for apache, php and mysql support

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,11 +1,41 @@
 exec { "apt-get update":
   path => "/usr/bin",
 }
-package { "apache2":
-  ensure  => present,
-  require => Exec["apt-get update"],
+class apache {
+  package { "apache2":
+    ensure  => present,
+    require => Exec["apt-get update"],
+  }
+  service { "apache2":
+    ensure  => "running",
+    require => Package["apache2"],
+  }
 }
-service { "apache2":
-  ensure  => "running",
-  require => Package["apache2"],
+class php {
+  package { "php5":
+    ensure  => present,
+  }
+  package { "php5-cli":
+    ensure  => present,
+  }
+  package { "php5-mysql":
+    ensure  => present,
+  }
+}
+class mysql {
+  package { "mysql-server":
+    ensure  => present,
+  }
+  service { "mysql":
+    ensure => running,
+    require => Package["mysql-server"],
+  }
+  exec { "set-mysql-password":
+    path    => ["/bin", "/usr/bin"],
+    command => "mysqladmin -uroot password root",
+    require => Service["mysql"],
+  }
+  #package { "phpmyadmin":
+  #  ensure  => present,
+  #}
 }
